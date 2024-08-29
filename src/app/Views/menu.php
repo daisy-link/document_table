41448faf593a0757ed1492bb342d077e6ee127d3
disk <dendou@daisy.link>
2024-08-29 12:36:32 +0900

    menuの変更

--- src/app/Views/menu.php
+++ src/app/Views/menu.php
@@ -35,6 +35,9 @@
             <li><a href="<?php echo ROOT_PATH; ?>setting/addition"><span>Csv Edit</span></a></li>
         </ul>
     </li>
+</ul>
+<hr>
+<ul>
     <li><a href="<?php echo ROOT_PATH; ?>setting/connect"><i>-</i><span>DB connect</span></a></li>
     <li><a href="<?php echo ROOT_PATH; ?>setting/inout"><i>-</i><span>Import Export</span></a></li>
 </ul>
